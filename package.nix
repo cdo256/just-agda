f525e3e0e7ced0476cb7acdbaeaeea2dca234908
Christina O'Donnell <cdo@mutix.org>
2025-03-19 21:45:29 +0000

    Include emacs packages in package.nix.

--- package.nix
+++ package.nix
@@ -1,4 +1,9 @@
-{ pkgs, replaceVars, ... }:
+{
+  pkgs,
+  replaceVars,
+  emacsPackagesFor,
+  ...
+}:
 let
   init-file = pkgs.stdenv.mkDerivation {
     name = "init.el";
@@ -8,19 +13,50 @@ let
       cp $src $out
     '';
   };
+  emacs = (emacsPackagesFor pkgs.emacs).emacsWithPackages (epkgs: [
+    epkgs.all-the-icons
+    epkgs.all-the-icons-dired
+    epkgs.command-log-mode
+    epkgs.company
+    epkgs.company-box
+    epkgs.counsel
+    epkgs.counsel-projectile
+    epkgs.dired
+    epkgs.dired-hide-dotfiles
+    epkgs.dired-open
+    epkgs.dired-single
+    epkgs.doom-modeline
+    epkgs.doom-themes
+    epkgs.evil
+    epkgs.evil-collection
+    epkgs.general
+    epkgs.helpful
+    epkgs.ivy
+    epkgs.ivy-prescient
+    epkgs.ivy-rich
+    epkgs.lsp-ivy
+    epkgs.lsp-mode
+    epkgs.lsp-treemacs
+    epkgs.lsp-ui
+    epkgs.no-littering
+    epkgs.projectile
+    epkgs.rainbow-delimiters
+    epkgs.visual-fill-column
+    epkgs.which-key
+  ]);
   wrapped = pkgs.writeShellScriptBin "just-agda" ''
     CONFIG_DIR="''${XDG_CONFIG_HOME:-''$HOME/.config}/just-agda"
     mkdir -p $CONFIG_DIR
     rm -f $CONFIG_DIR/init.el
     ln -s ${init-file} $CONFIG_DIR/init.el
-    ${pkgs.emacs}/bin/emacs --init-directory $CONFIG_DIR "$@"
+    ${emacs}/bin/emacs --init-directory $CONFIG_DIR "$@"
   '';
 in
 # See nix Cookbook.
 pkgs.symlinkJoin {
   name = "just-agda";
   paths = [
-    pkgs.emacs
+    emacs
     (pkgs.agda.withPackages (ps: [
       ps.standard-library
     ]))
