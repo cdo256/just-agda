396514159a78913e4604ebe47a9ffdf7271a164c
Christina O'Donnell <cdo@mutix.org>
2025-06-02 20:51:08 +0100

    set evil-shift-width.

--- init.el
+++ init.el
@@ -158,7 +158,8 @@
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
   (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
+  (evil-set-initial-state 'dashboard-mode 'normal)
+  (setq evil-shift-width 2))
 
 (use-package evil-collection
   :after evil
