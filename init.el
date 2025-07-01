ea5d34fff2a89303cb9d3bb6c7a656ea73e2b972
Christina O'Donnell <cdo@mutix.org>
2025-07-01 21:13:47 +0100

    Add evil shortcuts for changing windows.

--- init.el
+++ init.el
@@ -733,3 +733,21 @@
 
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
+
+(define-key evil-normal-state-map (kbd "C-<left>")  'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-<down>")  'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-<up>")    'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-<right>") 'evil-window-right)
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
+(define-key evil-motion-state-map (kbd "C-<left>")  'evil-window-left)
+(define-key evil-motion-state-map (kbd "C-<down>")  'evil-window-down)
+(define-key evil-motion-state-map (kbd "C-<up>")    'evil-window-up)
+(define-key evil-motion-state-map (kbd "C-<right>") 'evil-window-right)
+(define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
